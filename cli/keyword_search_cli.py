1a7f820fcbe455ae1b5d9c7d751525de98598259
Marin Peric Radan <marin.peric@seekandhit.com>
2025-10-21 15:00:40 +0200

    chore: additional search logic

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+
+
+def keyword_search(keyword, sourceFile):
+    results = []
+
+    with open(sourceFile, "r") as file:
+        data = json.load(file, strict=False)
+
+    for movie in data["movies"]:
+        if keyword in movie["title"]:
+            results.append(movie)
+
+    return results
 
 
 def main() -> None:
@@ -15,6 +29,12 @@ def main() -> None:
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
+            matching_movies = keyword_search(args.query, "data/movies.json")
+
+            count = 1
+            for movie in matching_movies:
+                print(f"{count}. Movie title {movie["title"]}")
+                count += 1
             pass
         case _:
             parser.print_help()
