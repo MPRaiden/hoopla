0cfab04085b2db7b6e83d824663f46555d4eec5b
Marin Peric Radan <marin.peric@seekandhit.com>
2025-10-29 14:26:13 +0100

    chore: update keyword_search to remove all punctuation

--- cli/lib/utils.py
+++ cli/lib/utils.py
@@ -1,4 +1,5 @@
 import json
+import string
 
 
 def keyword_search(keyword, sourceFile):
@@ -8,9 +9,20 @@ def keyword_search(keyword, sourceFile):
         data = json.load(file, strict=False)
 
     for movie in data["movies"]:
-        if keyword.lower() in movie["title"].lower():
+        clean_keyword = search_input_cleanup(keyword)
+        clean_title = search_input_cleanup(movie["title"])
+
+        if clean_keyword in clean_title:
             results.append(movie)
 
     sorted_results = sorted(results, key=lambda m: m["id"])
 
     return sorted_results[0:5]
+
+
+def search_input_cleanup(input):
+    input = input.lower()
+    remove_punc = str.maketrans("", "", string.punctuation)
+    input = input.translate(remove_punc)
+
+    return input
