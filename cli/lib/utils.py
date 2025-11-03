5d89a7c7698f97de0869d45fc7a2df24b7108643
Marin Peric Radan <marin.peric@seekandhit.com>
2025-11-03 14:20:26 +0100

    feat: logic to remove stopwords

--- cli/lib/utils.py
+++ cli/lib/utils.py
@@ -3,34 +3,27 @@ import string
 
 
 def keyword_search(keyword, sourceFile):
-    results = []
+    stopwords = get_stopwords("data/stopwords.txt")
 
     with open(sourceFile, "r") as file:
         data = json.load(file, strict=False)
 
+    # Clean query ONCE before the loop
+    clean_query = search_input_cleanup(keyword, stopwords)
+
+    results = []
     for movie in data["movies"]:
-        clean_query = search_input_cleanup(keyword)
-        clean_title = search_input_cleanup(movie["title"])
+        clean_title = search_input_cleanup(movie["title"], stopwords)
 
-        # checks if the title contains the query
-        found_match = False
-
-        for token in clean_query:
-            for title in clean_title:
-                if token in title:
-                    found_match = True
-                    break
-            if found_match:
-                break
-        if found_match:
+        if any(
+            token in title_token for token in clean_query for title_token in clean_title
+        ):
             results.append(movie)
 
-    sorted_results = sorted(results, key=lambda m: m["id"])
-
-    return sorted_results[0:5]
+    return sorted(results, key=lambda m: m["id"])[:5]
 
 
-def search_input_cleanup(input):
+def search_input_cleanup(input, stopwords=None):
     # handles casing
     input = input.lower()
 
@@ -42,4 +35,13 @@ def search_input_cleanup(input):
     tokens = input.split(" ")
     tokens = [token for token in tokens if token != ""]
 
+    # removes stopwords
+    if stopwords:
+        tokens = [token for token in tokens if token not in stopwords]
+
     return tokens
+
+
+def get_stopwords(file):
+    with open(file, "r") as f:
+        return set(f.read().splitlines())
